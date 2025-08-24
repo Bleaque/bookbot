52c6790f4d51e4cbdedfd3256b54da51b2d0a157
Bleaque <jackcollen@gmail.com>
2025-08-24 10:12:22 +0200

    pre extra books

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+import sys
 from stats import num_words
 from stats import get_book_text
+from stats import count_letters
+from stats import make_pretty
+print(sys.argv)
 
 def main():
-	a = "books/frankenstein.txt"
+	if len(sys.argv) != 2:
+                print("Usage: python3 main.py <path_to_book>")
+                sys.exit(1)	
+	a = sys.argv[1]
 	bookcontents = get_book_text(a)
+	print("============ BOOKBOT ============")
+	print(f"Analyzing book found at {a}...")
 	test = num_words(bookcontents)
-	print(f"{test} words found in the document")
-
+	print("----------- Word Count ----------")
+	print(f"Found {test} total words")
+	print("--------- Character Count -------")
+	cletter = count_letters(bookcontents)
+	x=make_pretty(cletter)
+	for items in x:
+		character_string = items["char"]
+		if character_string.isalpha():
+			print(f'{items["char"]}: {items["num"]}')
+	print("============= END ===============")
 main()
+print(sys.argv)
--- stats.py
+++ stats.py
@@ -11,3 +11,25 @@ def num_words(bookcontents):
         for words in listofwords:
                 count += 1
         return count
+
+def count_letters(file_contents):
+	cletter = {}
+	for letter in file_contents:
+		x = letter.lower()
+		if x in cletter:
+			cletter[x] += 1
+		else:
+			cletter[x] = 1
+	return cletter
+
+def sort_on(items):
+	return items["num"]
+
+def make_pretty(cletter):
+	tidy_list=[]
+	for char, count in cletter.items():
+		tidy_dict = {"char":char, "num": count}
+		tidy_list.append(tidy_dict)
+	tidy_list.sort(reverse=True, key=sort_on)
+	return tidy_list
+				
